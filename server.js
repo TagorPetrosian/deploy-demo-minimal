1c3cb6e10d5d4010692975968415c03474786fe1
Tagor Petrosian <tagorp@trufflelive.com>
2020-12-30 10:01:40 +0200

    deploy setup fixes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/demo-deploy-mini'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/demo-deploy-mini/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
